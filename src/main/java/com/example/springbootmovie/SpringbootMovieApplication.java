c86417eb9bac98c5f46fc80fb2b386caac96454a
chaabEMsi <Davysteve.Nacrochaabane@emsi-edu.ma>
2023-05-30 11:09:32 +0100

    first commit

---
+++ src/main/java/com/example/springbootmovie/SpringbootMovieApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMovieApplication.class, args);
+	}
+}
